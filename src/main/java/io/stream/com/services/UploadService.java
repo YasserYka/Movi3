d97cfa9a682cb129d530aa1eafd626fdb5cf4982
YasserYka <yasseryka@gmail.com>
2020-04-08 22:33:55 +0300

    media util class

--- src/main/java/io/stream/com/services/UploadService.java
+++ src/main/java/io/stream/com/services/UploadService.java
@@ -1,9 +1,15 @@
 package io.stream.com.services;
 
 import io.stream.com.repositories.MovieRepository;
+import io.stream.com.utils.MediaUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.HashMap;
+
+@Slf4j
 @Service
 public class UploadService {
 
@@ -13,5 +19,20 @@ public class UploadService {
     @Autowired
     private S3Client s3Client;
 
+    private HashMap supportedFormats;
 
+    public void upload(MultipartFile multipartFile){
+
+        if(isNotSupported(multipartFile.getOriginalFilename()))
+            return;
+
+
+    }
+
+    private boolean isNotSupported(String filename) {
+        if(MediaUtil.isFormatSupported(filename))
+            return false;
+        log.warn("File With Not Supported Format Was Uploaded {}", filename);
+        return true;
+    }
 }
