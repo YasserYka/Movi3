09cd71bc7c49c5e43b348631fb61419d3ce3bc20
YasserYka <yasseryka@gmail.com>
2020-04-08 12:29:55 +0300

    cleaning up to fit both branches

--- src/main/java/io/stream/com/repositories/MovieRepository.java
+++ src/main/java/io/stream/com/repositories/MovieRepository.java
@@ -1,9 +1,3 @@
 package io.stream.com.repositories;
 
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import io.stream.com.models.Movie;
-
-public interface MovieRepository extends ReactiveMongoRepository<Movie, Long>{
-
-}
+public interface MovieRepository{}
--- src/main/java/io/stream/com/services/MovieService.java
+++ src/main/java/io/stream/com/services/MovieService.java
@@ -1,17 +1,6 @@
 package io.stream.com.services;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 
-import io.stream.com.models.Movie;
-
 @Service
-public class MovieService {
-
-	public Optional<Movie> getById() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-}
+public class MovieService {}
