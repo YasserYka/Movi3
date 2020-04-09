2f7a9c86dc68543c132d2c80fddba2a22f3d665e
YasserYka <yasseryka@gmail.com>
2020-04-09 19:05:52 +0300

    bashutil.class finished

--- src/main/java/io/stream/com/utils/BashUtil.java
+++ src/main/java/io/stream/com/utils/BashUtil.java
@@ -1,6 +1,10 @@
 package io.stream.com.utils;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class BashUtil {
 
@@ -10,23 +14,27 @@ public class BashUtil {
     @Value("${bash.extract.filename}")
     private static String bashExtractFilename;
 
-    //Takes
-    public static String[] buildCommand(ProcessType processType, String... args){
-
-        String[] command = new String[args.length + 2];
+    public static ProcessBuilder processBuilder(ProcessType processType, String... args){
+        String[] commands = new String[args.length + 2];
         int i = 0;
-        command[i++] = "/bin/bash";
-        command[i++] = pathOfBashScript(processType);
+        
+        commands[i++] = "/bin/bash";
+        commands[i++] = pathOfBashScript(processType);
 
-        for (String arg : args){ command[i++] = arg; }
+        for (String arg : args){ commands[i++] = arg; }
 
-        return command;
+        return new ProcessBuilder(commands);
     }
 
     private static String pathOfBashScript(ProcessType processType){
         if(processType == ProcessType.extract_audio)
             return bashPath + bashExtractFilename;
-
         return null;
+    }
+
+    @SneakyThrows(IOException.class)
+    public static InputStream runProcess(ProcessBuilder processBuilder){
+        Process process = processBuilder.start();
+        return process.getInputStream();
     }
 }
