85363fb42a1aac50b0c494720b3f262332894610
YasserYka <yasseryka@gmail.com>
2020-04-26 02:22:58 +0300

    finished like model

--- src/main/java/io/stream/com/models/Like.java
+++ src/main/java/io/stream/com/models/Like.java
@@ -1,5 +1,29 @@
 package io.stream.com.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Like {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="movieId", referencedColumnName="movieId")
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="userId", referencedColumnName = "userId")
+    private User user;
 
 }
