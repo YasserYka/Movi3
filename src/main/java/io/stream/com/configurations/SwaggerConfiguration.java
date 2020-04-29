7d9223b0be5ca4c6526845af86aa02ebfd583ae0
YasserYka <yasseryka@gmail.com>
2020-04-29 23:20:58 +0300

    swagger ui config

--- src/main/java/io/stream/com/configurations/SwaggerConfiguration.java
+++ src/main/java/io/stream/com/configurations/SwaggerConfiguration.java
@@ -1,9 +1,22 @@
 package io.stream.com.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration { }
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+                .paths(PathSelectors.ant("/api/v1/*"))
+                    .build();
+    }
+ }
