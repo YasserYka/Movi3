1964b3f31433aed77293408257a85ab292a41d31
YasserYka <yasseryka@gmail.com>
2020-04-30 22:51:37 +0300

    redis setup

---
+++ src/main/java/io/stream/com/configurations/RedisConfiguration.java
@@ -0,0 +1,36 @@
+package io.stream.com.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Value("${redis.hostname}")
+    private String hostname;
+
+    @Value("${redis.password}")
+    private String password;
+
+    @Value("${redis.port}")
+    private String port;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(hostname, port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
