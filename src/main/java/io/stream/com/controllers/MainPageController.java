80d3f1c346571efb9c9e996f316e143b92cc2152
YasserYka <yasseryka@gmail.com>
2020-04-30 19:38:08 +0300

    added mainpage controller and service setup

---
+++ src/main/java/io/stream/com/controllers/MainPageController.java
@@ -0,0 +1,43 @@
+package io.stream.com.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.stream.com.models.Movie;
+import io.stream.com.services.MainPageService;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/v1/beingwatchrightnow")
+public class MainPageController {
+
+
+    @Autowired
+    private MainPageService service;
+
+
+    @PostMapping("/beingwatchedrightnow")
+    public ResponseEntity<?> addMovieBeingWatched(){
+
+        
+        return new ResponseEntity<>(HttpStatus.CREATED);    
+    }
+
+    @GetMapping("/beingwatchedrightnow")
+    public ResponseEntity<Movie> get10MoviesBeingWatched(){
+
+        List<Movie> list = service.get10MoviesBeingWatched();
+        
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);    
+    }
+
+}
---
+++ src/main/java/io/stream/com/services/MainPageService.java
@@ -0,0 +1,14 @@
+package io.stream.com.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import io.stream.com.models.Movie;
+
+@Service
+public class MainPageService {
+	public List<Movie> get10MoviesBeingWatched() {
+		return null;
+	}
+}
