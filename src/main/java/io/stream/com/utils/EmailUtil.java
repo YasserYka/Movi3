9bedf7e07a92da64713922804fb95a4f50466fe2
YasserYka <yasseryka@gmail.com>
2020-05-17 19:21:53 +0300

    removed emailmapper and added uuid class

---
+++ src/main/java/io/stream/com/utils/EmailUtil.java
@@ -0,0 +1,18 @@
+package io.stream.com.utils;
+
+import org.springframework.mail.SimpleMailMessage;
+
+public class EmailUtil {
+
+    public static void verify(){
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setTo();
+        simpleMailMessage.setSubject();
+        simpleMailMessage.setText();
+        simpleMailMessage.setFrom();
+        
+    }
+    
+}
---
+++ src/main/java/io/stream/com/utils/KeyUtil.java
@@ -0,0 +1,9 @@
+package io.stream.com.utils;
+
+import java.util.UUID;
+
+public class KeyUtil {
+  
+        public String generate(){ return UUID.randomUUID().toString(); }
+
+}
