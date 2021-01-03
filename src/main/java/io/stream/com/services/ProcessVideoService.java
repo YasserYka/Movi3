ea50a1aba6426d5b60f90bb7ad9df1d5708aa365
YasserYka <yasseryka@gmail.com>
2021-01-03 14:52:47 +0300

    regex for parsing ffmpeg stdout and buffer reader

--- src/main/java/io/stream/com/services/ProcessVideoService.java
+++ src/main/java/io/stream/com/services/ProcessVideoService.java
@@ -1,6 +1,9 @@
 package io.stream.com.services;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
@@ -11,15 +14,29 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class ProcessVideoService {
 
+    Pattern estimatedDurationPattern = Pattern.compile("(Duration): (.*?),");
+    Pattern timeDurationPattern = Pattern.compile("(time)=(.*?)\s");
+
     private ProcessBuilder buildProcess(){
 
         return new ProcessBuilder("/bin/bash", "-c", "yes n | ffmpeg -i sample.mp4 output.avi 2>&1");
     }
 
+    private BufferedReader getBufferedReader(Process process){
+
+        return new BufferedReader(new InputStreamReader(process.getInputStream()));
+    }
+
     @SneakyThrows(IOException.class)
     private void process(){
+        
+        BufferedReader bufferedReader = getBufferedReader(buildProcess().start());
+        String bufferedLine;
 
-        buildProcess().start();
-        log.info("Processing started for ..");
+        while ((bufferedLine = bufferedReader.readLine()) != null) {
+            
+        }
+        
     }
+ 
 }
