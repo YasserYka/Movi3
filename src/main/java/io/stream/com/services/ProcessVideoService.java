b514cc746331d3f4fcc8205c14b33b44cd6dae7f
YasserYka <yasseryka@gmail.com>
2021-01-03 15:41:14 +0300

    impl logic for progress processing video

--- src/main/java/io/stream/com/services/ProcessVideoService.java
+++ src/main/java/io/stream/com/services/ProcessVideoService.java
@@ -3,6 +3,7 @@ package io.stream.com.services;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
@@ -14,8 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class ProcessVideoService {
 
-    Pattern estimatedDurationPattern = Pattern.compile("(Duration): (.*?),");
-    Pattern timeDurationPattern = Pattern.compile("(time)=(.*?)\s");
+    Pattern estimatedDurationPattern = Pattern.compile("Duration: (.*?),");
+    Pattern timeDurationPattern = Pattern.compile("time=(.*?)\s");
 
     private ProcessBuilder buildProcess(){
 
@@ -32,11 +33,30 @@ public class ProcessVideoService {
         
         BufferedReader bufferedReader = getBufferedReader(buildProcess().start());
         String bufferedLine;
+        double estimatedDuration = 0, currentDuration = 0;
 
         while ((bufferedLine = bufferedReader.readLine()) != null) {
             
+            Matcher estimatedDurationMatcher = estimatedDurationPattern.matcher(bufferedLine);
+            if (estimatedDurationMatcher.find())
+                estimatedDuration = parseDuration(estimatedDurationMatcher.group(2));
+
+            
+            Matcher timeDurationMatcher = timeDurationPattern.matcher(bufferedLine);
+            if (timeDurationMatcher.find())
+                currentDuration = parseDuration(timeDurationMatcher.group(2));
+
+            int progress = (int) ((currentDuration / estimatedDuration) * 100);
         }
         
+    }
+
+    // convert dd:dd:dd into seconds
+    private double parseDuration(String duration){
+
+        String[] durations = duration.split(":");
+
+        return Integer.parseInt(durations[0]) * 3600 + Integer.parseInt(durations[1]) * 60 + Double.parseDouble(durations[2]);
     }
  
 }
