0b1646c86c6524bdbe4c11749600519cf55e777d
YasserYka <yasseryka@gmail.com>
2021-01-06 20:05:46 +0300

    replaced sse config with thread config

--- src/main/java/io/stream/com/configurations/ThreadConfiguration.java
+++ src/main/java/io/stream/com/configurations/ThreadConfiguration.java
@@ -1,17 +1,27 @@
 package io.stream.com.configurations;
 
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SSEConfiguration {
+public class ThreadConfiguration {
  
     @Bean 
+    @Qualifier("sse-video-processing")
     public ScheduledExecutorService scheduledExecutorService(){
 
         return Executors.newScheduledThreadPool(1);
+    }
+
+    @Bean
+    @Qualifier("video-processing")
+    public ExecutorService singleThreadExecutor(){
+
+        return Executors.newSingleThreadExecutor();
     }
 }
