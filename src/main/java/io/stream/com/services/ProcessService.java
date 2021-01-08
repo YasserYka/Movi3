d927c0d0018afd2f35e5d56dcb6cded9e2dbcd1f
YasserYka <yasseryka@gmail.com>
2021-01-08 19:43:53 +0300

    redid process service

--- src/main/java/io/stream/com/services/ProcessService.java
+++ src/main/java/io/stream/com/services/ProcessService.java
@@ -2,59 +2,65 @@ package io.stream.com.services;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import io.stream.com.components.MQReceiver;
 import io.stream.com.models.dtos.MQMessage;
+import io.stream.com.models.dtos.ProcessStatus;
 import io.stream.com.models.enums.MQEvent;
+import io.stream.com.utils.BashUtil;
 import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 public class ProcessService {
 
-    Pattern estimatedDurationPattern = Pattern.compile("Duration: (.*?),");
-    Pattern timeDurationPattern = Pattern.compile("time=(.*?) ");
+    private static final Pattern ESTIMATED_DURATION_PATTERN = Pattern.compile("Duration: (.*?),");
+    private static final Pattern TIME_DURATION_PATTERN = Pattern.compile("time=(.*?) ");
 
-    private ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
-
-    private ProcessBuilder buildProcess(){
-
-        return new ProcessBuilder("/bin/bash", "-c", "yes y | ffmpeg -i sample_720.mp4 output.avi 2>&1");
-    }
-
-    private BufferedReader getBufferedReader(Process process){
-
-        return new BufferedReader(new InputStreamReader(process.getInputStream()));
-    }
+    private static final ExecutorService SINGLE_THREAD_EXECUTOR = Executors.newSingleThreadExecutor();
+    private static ProcessStatus PROCESS_STATUS = new ProcessStatus(0, "Null", 0);
 
     public void process(MQMessage message){
 
-        singleThreadExecutor.execute(() -> {
+        SINGLE_THREAD_EXECUTOR.execute(() -> {
             
             processVideo(message.getFilename(), message.getFilepath(), message.getEvent());
         });
                 
     }
 
+    @SneakyThrows(IOException.class)
     private void processVideo(String filename, String path, MQEvent event){
 
-        
-    } 
+        BufferedReader bufferedReader = BashUtil.getBufferedReader(BashUtil.buildProcessFor(event).start());
+    
+        String bufferedLine;
+        double estimatedDuration = 0, currentDuration = 0;
 
+        PROCESS_STATUS.setFilename(filename);
 
+        while ((bufferedLine = bufferedReader.readLine()) != null) {
+            
+            Matcher estimatedDurationMatcher = ESTIMATED_DURATION_PATTERN.matcher(bufferedLine);
+            if (estimatedDurationMatcher.find()){
+                estimatedDuration = parseDuration(estimatedDurationMatcher.group(1));
+                PROCESS_STATUS.setEstimatedTime((int) estimatedDuration);
+            }
+            
+            Matcher timeDurationMatcher = TIME_DURATION_PATTERN.matcher(bufferedLine);
+            if (timeDurationMatcher.find())
+                currentDuration = parseDuration(timeDurationMatcher.group(1));
 
-    // convert dd:dd:dd into seconds
+            PROCESS_STATUS.setPercentage((int) ((currentDuration / estimatedDuration) * 100));
+        }
+
+    }
+
+    // convert dd:dd:dd string into seconds
     private double parseDuration(String duration){
 
         String[] durations = duration.split(":");
