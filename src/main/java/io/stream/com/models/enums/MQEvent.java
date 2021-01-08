e7687ee26f94b3ee22d0dae17a57a8c81676afff
YasserYka <yasseryka@gmail.com>
2021-01-08 14:28:22 +0300

    replaced processing enum w mqmessage

---
+++ src/main/java/io/stream/com/models/enums/MQEvent.java
@@ -0,0 +1,11 @@
+package io.stream.com.models.enums;
+
+public enum MQEvent {
+
+    TO_720p,
+    TO_480p,
+    TO_360p,
+    EXTRACT_AUDIO,
+    GENERATE_MANIFEST;
+    
+}
--- src/main/java/io/stream/com/models/enums/VideoProcessType.java
+++
@@ -1,7 +0,0 @@
-package io.stream.com.models.enums;
-
-public enum VideoProcessType {
-    extract_audio,
-    convert,
-    generate_manifest
-}
